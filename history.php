3062df799c08e4d09abed0e4d300298fb7752f7e
diarrabell <dbell5@u.rochester.edu>
2018-12-03 15:58:45 -0500

    removed extra folder

---
+++ history.php
@@ -0,0 +1,18 @@
+<html lang=en>
+    <head>
+        <title>History of Space Travel</title>  
+    </head>
+    
+    <h1>History of Space Travel</h1>
+    <p>
+        The first theoretical proposal of space travel using rockets was published by Scottish astronomer and mathematician William Leitch, in an 1861 essay "A Journey Through Space".[1] More well-known (though not widely outside Russia) is Konstantin Tsiolkovsky's work, "Исследование мировых пространств реактивными приборами" (The Exploration of Cosmic Space by Means of Reaction Devices), published in 1903.
+
+        Spaceflight became an engineering possibility with the work of Robert H. Goddard's publication in 1919 of his paper A Method of Reaching Extreme Altitudes. His application of the de Laval nozzle to liquid fuel rockets improved efficiency enough for interplanetary travel to become possible. He also proved in the laboratory that rockets would work in the vacuum of space;[specify] nonetheless, his work was not taken seriously by the public. His attempt to secure an Army contract for a rocket-propelled weapon in the first World War was defeated by the November 11, 1918 armistice with Germany.
+
+        Nonetheless, Goddard's paper was highly influential on Hermann Oberth, who in turn influenced Wernher von Braun. Von Braun became the first to produce modern rockets as guided weapons, employed by Adolf Hitler. Von Braun's V-2 was the first rocket to reach space, at an altitude of 189 kilometers (102 nautical miles) on a June 1944 test flight.[2]
+
+        Tsiolkovsky's rocketry work was not fully appreciated in his lifetime, but he influenced Sergey Korolev, who became the Soviet Union's chief rocket designer under Joseph Stalin, to develop intercontinental ballistic missiles to carry nuclear weapons as a counter measure to United States bomber planes. Derivatives of Korolev's R-7 Semyorka missiles were used to launch the world's first artificial Earth satellite, Sputnik 1, on October 4, 1957, and later the first human to orbit the Earth, Yuri Gagarin in Vostok 1, on April 12, 1961.[3]
+
+        At the end of World War II, von Braun and most of his rocket team surrendered to the United States, and were expatriated to work on American missiles at what became the Army Ballistic Missile Agency. This work on missiles such as Juno I and Atlas enabled launch of the first US satellite Explorer 1 on February 1, 1958, and the first American in orbit, John Glenn in Friendship 7 on February 20, 1962. As director of the Marshall Space Flight Center, Von Braun oversaw development of a larger class of rocket called Saturn, which allowed the US to send the first two humans, Neil Armstrong and Buzz Aldrin, to the Moon and back on Apollo 11 in July 1969. Over the same period, the Soviet Union secretly tried but failed to develop the N1 rocket to give them the capability to land one person on the Moon.
+    </p>
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<html lang=en>
+    <head>
+        <title>Space travel</title>
+    </head>
+    
+    <h1>Space Travel</h1>
+    <p>
+        Spaceflight (also written space flight) is ballistic flight into or through outer space. Spaceflight can occur with spacecraft with or without humans on board. Examples of human spaceflight include the U.S. Apollo Moon landing and Space Shuttle programs and the Russian Soyuz program, as well as the ongoing International Space Station. Examples of unmanned spaceflight include space probes that leave Earth orbit, as well as satellites in orbit around Earth, such as communications satellites. These operate either by telerobotic control or are fully autonomous.
+
+        Spaceflight is used in space exploration, and also in commercial activities like space tourism and satellite telecommunications. Additional non-commercial uses of spaceflight include space observatories, reconnaissance satellites and other Earth observation satellites.
+
+        A spaceflight typically begins with a rocket launch, which provides the initial thrust to overcome the force of gravity and propels the spacecraft from the surface of the Earth. Once in space, the motion of a spacecraft – both when unpropelled and when under propulsion – is covered by the area of study called astrodynamics. Some spacecraft remain in space indefinitely, some disintegrate during atmospheric reentry, and others reach a planetary or lunar surface for landing or impact.
+    </p>
+    
+    <h2>Types of spaceflight</h2>
+    
+    <h3>Uncrewed</h3>
+    <p>
+        Uncrewed spaceflight (or unmanned) is all spaceflight activity without a necessary human presence in space. This includes all space probes, satellites and robotic spacecraft and missions. Uncrewed spaceflight is the opposite of manned spaceflight, which is usually called human spaceflight. Subcategories of uncrewed spaceflight are "robotic spacecraft" (objects) and "robotic space missions" (activities). A robotic spacecraft is an uncrewed spacecraft with no humans on board, that is usually under telerobotic control. A robotic spacecraft designed to make scientific research measurements is often called a space probe.
+
+        Uncrewed space missions use remote-controlled spacecraft. The first uncrewed space mission was Sputnik I, launched October 4, 1957 to orbit the Earth. Space missions where animals but no humans are on-board are considered uncrewed missions.
+    </p>
+    
+    <h3>Human</h3>
+    <p>
+        The first human spaceflight was Vostok 1 on April 12, 1961, on which cosmonaut Yuri Gagarin of the USSR made one orbit around the Earth. In official Soviet documents, there is no mention of the fact that Gagarin parachuted the final seven miles.[9] Currently, the only spacecraft regularly used for human spaceflight are the Russian Soyuz spacecraft and the Chinese Shenzhou spacecraft. The U.S. Space Shuttle fleet operated from April 1981 until July 2011. SpaceShipOne has conducted two human suborbital spaceflights.
+    </p>
+    
+    <footer>
+        <h4>References: Wikipedia</h4>
+        <a href="https://en.wikipedia.org/wiki/Spaceflight">Spaceflight</a>
+    </footer>
+
+</html>
+
+    
+    
+    
